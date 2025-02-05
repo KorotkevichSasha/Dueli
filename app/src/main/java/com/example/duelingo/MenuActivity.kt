4a69247e16dc3fd8e26b9089eb839fe0f7f92656
Sasha <torsach33@gmail.com>
2025-02-05 22:26:47 +0300

    v0.1 Remove navigation bar and fragments

--- app/src/main/java/com/example/duelingo/MenuActivity.kt
+++ app/src/main/java/com/example/duelingo/MenuActivity.kt
@@ -1,29 +1,117 @@
 package com.example.duelingo
 
+import android.animation.Animator
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.core.content.ContextCompat
+import com.airbnb.lottie.LottieAnimationView
 import com.example.duelingo.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMenuBinding
-    private lateinit var navController: NavController
+    private var currentAnimationView: LottieAnimationView? = null
+    private var currentIcon: ImageView? = null
+    private var currentText: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
-        val navController = navHostFragment.navController
-        binding.bottomNavigation.setupWithNavController(navController)
+        binding.btnDuel.setOnClickListener {
+            showToast("Поиск дуэли...")
+        }
+
+        binding.test.setOnClickListener {
+            resetAll();
+            changeColorAndIcon(binding.testIcon, binding.testTest, R.drawable.grad)
+            playAnimation(binding.testAnimation, binding.testIcon, binding.testTest, "graAnim.json")
+        }
+
+        binding.main.setOnClickListener {
+            resetAll();
+            changeColorAndIcon(binding.mainIcon, binding.mainTest, R.drawable.swo)
+            playAnimation(binding.duelAnimation, binding.mainIcon, binding.mainTest, "swordAnim.json")
+        }
+
+        binding.cup.setOnClickListener {
+            resetAll();
+            changeColorAndIcon(binding.cupIcon, binding.cupTest, R.drawable.tro)
+            playAnimation(binding.cupAnimation, binding.cupIcon, binding.cupTest, "cupAnim.json")
+        }
+
+        binding.profile.setOnClickListener {
+            resetAll();
+            changeColorAndIcon(binding.profileIcon, binding.profileTest, R.drawable.prof)
+            playAnimation(binding.profAnimation, binding.profileIcon, binding.profileTest, "profAnim.json")
+        }
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun changeColorAndIcon(icon: ImageView, text: TextView, iconRes: Int) {
+        text.setTextColor(ContextCompat.getColor(this, R.color.blue_primary))
+        icon.setImageResource(iconRes)
+    }
+
+    private fun playAnimation(animationView: LottieAnimationView, icon: ImageView, text: TextView, animationFile: String) {
+        currentAnimationView?.apply {
+            cancelAnimation()
+            visibility = View.GONE
+        }
+        currentIcon?.visibility = View.VISIBLE
+        currentText?.setTextColor(Color.parseColor("#7A7A7B"))
+
+        currentAnimationView = animationView
+        currentIcon = icon
+        currentText = text
+
+        icon.visibility = View.GONE
+
+        animationView.visibility = View.VISIBLE
+        animationView.setAnimation(animationFile)
+        animationView.playAnimation()
+
+        animationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                icon.visibility = View.VISIBLE
+                animationView.visibility = View.GONE
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                icon.visibility = View.VISIBLE
+                animationView.visibility = View.GONE
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+            private fun playAnimation(animationFile: String) {
+                binding.animationView.setAnimation(animationFile)
+                binding.animationView.playAnimation()
+            }
+        })
+    }
+    private fun resetAll() {
+        binding.testTest.setTextColor(Color.parseColor("#7A7A7B"))
+        binding.mainTest.setTextColor(Color.parseColor("#7A7A7B"))
+        binding.cupTest.setTextColor(Color.parseColor("#7A7A7B"))
+        binding.profileTest.setTextColor(Color.parseColor("#7A7A7B"))
+
+        binding.testIcon.setImageResource(R.drawable.graduation24)
+        binding.mainIcon.setImageResource(R.drawable.swords24)
+        binding.cupIcon.setImageResource(R.drawable.trophy24)
+        binding.profileIcon.setImageResource(R.drawable.profile24)
     }
 }
